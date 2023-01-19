0803124a323cf7df411c6f1f415d64a3c3b8ff2b
Ramya Gunashekara <ramya.gunashekar@gmail.com>
2023-01-19 10:33:28 +1000

    Delete auction directory

--- auction/auction.query/src/main/java/com/fse3/auction/query/domain/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fse3.auction.query.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface ProductRepository extends CrudRepository<Product, String> {
-    Optional<Product> findById(String id);
-}
